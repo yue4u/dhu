bbeb1c8b10d91a26a9696a5776db612f2d5bda8b
rainy-me <github@rainy.me>
2020-04-22 02:15:42 +0900

    save user info to data dir

--- packages/cli/index.ts
+++ packages/cli/index.ts
@@ -1,26 +1,16 @@
-import dotenv from "dotenv";
-dotenv.config();
 import cac from "cac";
 import { login, getAttendance } from "@dhu/core";
 import { renderAttendance } from "./view";
-const { DIGICAM_ID, DIGICAM_PASSWORD } = process.env;
-if (!DIGICAM_ID || !DIGICAM_PASSWORD) {
-  throw Error("no login info");
-}
 
-(async () => {
-  const cli = cac();
-  cli.command("atte", "get attendance").action(async () => {
-    const { page, browser } = await login({
-      id: DIGICAM_ID,
-      password: DIGICAM_PASSWORD,
-    });
-    const data = await getAttendance(page);
-    renderAttendance(data);
-    await browser.close();
-  });
+const cli = cac();
 
-  cli.help();
-  cli.version("0.0.1");
-  cli.parse();
-})();
+cli.command("atte", "get attendance").action(async () => {
+  const { page, browser } = await login();
+  const data = await getAttendance(page);
+  renderAttendance(data);
+  await browser.close();
+});
+
+cli.help();
+cli.version("0.0.1");
+cli.parse();
