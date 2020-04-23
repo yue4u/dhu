a22471be38bdb04dbb876113c64aa6ce26f307ed
rainy-me <github@rainy.me>
2020-04-24 01:21:43 +0900

    refact: change seletor format

--- packages/core/login.ts
+++ packages/core/login.ts
@@ -3,7 +3,12 @@ import {
   BrowserTypeLaunchOptions,
   ChromiumBrowserContext,
 } from "playwright";
-import { LOGIN, URL } from "./selectors";
+import {
+  LOGIN_ID,
+  LOGIN_PASSWORD,
+  LOGIN_SUBMIT_BUTTON,
+  URL_TOP,
+} from "./selectors";
 import { getUserInfo, removeUserInfo } from "./userInfo";
 
 export async function login(option?: BrowserTypeLaunchOptions) {
@@ -11,18 +16,18 @@ export async function login(option?: BrowserTypeLaunchOptions) {
   const browser = await chromium.launch(option);
   const ctx = await browser.newContext();
   const page = await ctx.newPage();
-  await page.goto(URL.TOP);
+  await page.goto(URL_TOP);
   //await exposeGlobalHelper(ctx);
 
   // if(maintenance){
   //   // TODO
   // }
 
-  await page.type(LOGIN.ID, id);
-  await page.type(LOGIN.PASSWORD, password);
+  await page.type(LOGIN_ID, id);
+  await page.type(LOGIN_PASSWORD, password);
   await Promise.all([
     page.waitForNavigation(),
-    page.click(LOGIN.SUBMIT_BUTTON),
+    page.click(LOGIN_SUBMIT_BUTTON),
   ]);
   const err = await page.evaluate(() => {
     const e = document.querySelector(".ui-messages-error-detail");
--- packages/core/selectors.ts
+++ packages/core/selectors.ts
@@ -1,21 +1,13 @@
-export const URL = {
-  TOP: `https://portal.dhw.ac.jp/uprx/`,
-};
+export const URL_TOP = `https://portal.dhw.ac.jp/uprx/`;
 
-export const LOGIN = {
-  ID: `#loginForm\\:userId`,
-  PASSWORD: `#loginForm\\:password`,
-  SUBMIT_BUTTON: `#loginForm\\:loginButton`,
-} as const;
+export const LOGIN_ID = `#loginForm\\:userId`;
+export const LOGIN_PASSWORD = `#loginForm\\:password`;
+export const LOGIN_SUBMIT_BUTTON = `#loginForm\\:loginButton`;
 
-export const CHANGE_PASSWORD = {
-  CURRENT: `#funcForm\\:password`,
-  NEW: `#funcForm\\:j_idt174`,
-  NEW_CONFIRM: `#funcForm\\:j_idt179`,
-  SUBMIT_BUTTON: `#funcForm\\:j_idt198`,
-} as const;
+export const CHANGE_PASSWORD_CURRENT = `#funcForm\\:password`;
+export const CHANGE_PASSWORD_NEW = `#funcForm\\:j_idt174`;
+export const CHANGE_PASSWORD_NEW_CONFIRM = `#funcForm\\:j_idt179`;
+export const CHANGE_PASSWORD_SUBMIT_BUTTON = `#funcForm\\:j_idt198`;
 
-export const NAV = {
-  TAB: `#menuForm\\:mainMenu > ul > li:nth-child(2) > a`,
-  ATTENDANCE: `#menuForm\\:mainMenu > ul > li.ui-widget.ui-menuitem.ui-corner-all.ui-menu-parent.ui-menuitem-active > ul > table > tbody > tr > td:nth-child(1) > ul > li.ui-menuitem.ui-widget.ui-corner-all > a`,
-} as const;
+export const NAV_TAB = `#menuForm\\:mainMenu > ul > li:nth-child(2) > a`;
+export const NAV_ATTENDANCE = `#menuForm\\:mainMenu > ul > li.ui-widget.ui-menuitem.ui-corner-all.ui-menu-parent.ui-menuitem-active > ul > table > tbody > tr > td:nth-child(1) > ul > li.ui-menuitem.ui-widget.ui-corner-all > a`;
