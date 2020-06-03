b551e12970c996d6f0737117ce637615bffa442a
rainy-me <github@rainy.me>
2020-06-04 03:26:32 +0900

    fix: function data importing

--- packages/syllabus-site/functions/search.js
+++ packages/syllabus-site/functions/search.js
@@ -1,4 +1,4 @@
-const { data } = require("./search.json");
+const data = require("./search.json");
 
 exports.handler = function (event, context, callback) {
   const params = event.queryStringParameters;
@@ -6,17 +6,17 @@ exports.handler = function (event, context, callback) {
 
   let result = {
     more: false,
-    data: []
-  }
+    data: [],
+  };
   if (!searchText) {
     callback(undefined, {
       statusCode: 200,
-      body: JSON.stringify(result)
+      body: JSON.stringify(result),
     });
   }
 
   const searchTextLower = searchText.toLowerCase();
-  let results = data.filter(item => {
+  let results = data.filter((item) => {
     item.index = item.text.toLowerCase().indexOf(searchTextLower);
 
     return item.index !== -1;
@@ -24,18 +24,20 @@ exports.handler = function (event, context, callback) {
 
   result.more = results.length > 15;
 
-  result.data = results.splice(0, 15).map(item => {
-    const text = item.text
-      .substring(item.index - 10, item.index + searchText.length + 15)
+  result.data = results.splice(0, 15).map((item) => {
+    const text = item.text.substring(
+      item.index - 10,
+      item.index + searchText.length + 15
+    );
     return {
       text: `...${text}...`,
       title: item.title,
-      url: `/${item.type}/${item.id}`
+      url: `/${item.type}/${item.id}`,
     };
   });
 
   callback(undefined, {
     statusCode: 200,
-    body: JSON.stringify(result)
+    body: JSON.stringify(result),
   });
 };
