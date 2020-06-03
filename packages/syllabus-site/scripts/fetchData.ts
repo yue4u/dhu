881328a1021b13ccf2ec55d5562f938808eecdba
rainy-me <github@rainy.me>
2020-06-04 06:43:00 +0900

    chore: format and sort data better

--- packages/syllabus-site/scripts/fetchData.ts
+++ packages/syllabus-site/scripts/fetchData.ts
@@ -1,12 +1,13 @@
 import path from "path";
 import { promises as fs } from "fs";
 import { getSyllabus, withPage } from "@dhu/core";
+import { sortData } from "./sortData";
 
 (async () => {
   const data = await withPage(getSyllabus);
   await fs.writeFile(
     path.join(__dirname, "../data/syllabus.json"),
-    JSON.stringify(data, null, 4),
+    JSON.stringify(sortData(data), null, 4),
     {
       encoding: "utf8",
     }
--- packages/syllabus-site/scripts/sortData.ts
+++ packages/syllabus-site/scripts/sortData.ts
@@ -1,9 +1,14 @@
 import path from "path";
+import type { Course } from "@dhu/core";
 import { promises as fs } from "fs";
 import syllabusData from "../data/syllabus.json";
 
+export const sortData = (data: Course[]) => {
+  return data.sort((a, b) => Number(a.code > b.code));
+};
+
 (async () => {
-  const data = syllabusData.sort((a, b) => Number(a.code > b.code));
+  const data = sortData(syllabusData);
   await fs.writeFile(
     path.join(__dirname, "../data/syllabus.json"),
     JSON.stringify(data, null, 4),
