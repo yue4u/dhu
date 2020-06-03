b7c867c22a788a337433f45ef0162c6dfbf41767
rainy-me <github@rainy.me>
2020-06-04 05:29:59 +0900

    query and display more data

--- packages/syllabus-site/src/templates/Course.vue
+++ packages/syllabus-site/src/templates/Course.vue
@@ -16,20 +16,30 @@
 
       <syllabus-button :link="link" v-for="(link, id) in tags" :key="id" />
 
+      <syllabus-section
+        title="Requirements"
+        :content="$page.course.requirements"
+      />
+      <syllabus-section
+        title="requirements Adjusted"
+        :content="$page.course.requirementsAdjusted"
+      />
+      <syllabus-section title="Contact" :content="$page.course.contact" />
       <syllabus-section title="Purpose" :content="$page.course.purpose" />
       <syllabus-section title="Target" :content="$page.course.target" />
       <!-- <syllabus-section
         title="Teaching Style"
         :content="$page.course.teachingStyle"
       /> -->
+      <syllabus-section title="Contents" :content="$page.course.contents" />
+      <syllabus-section title="References" :content="$page.course.references" />
+      <syllabus-section title="Final Test" :content="$page.course.task" />
+      <syllabus-section title="Task" :content="$page.course.finalTest" />
       <syllabus-section
         title="Grade Policy"
         :content="$page.course.gradePolicy"
       />
-      <syllabus-section title="Final Test" :content="$page.course.finalTest" />
       <syllabus-section title="Message" :content="$page.course.message" />
-      <syllabus-section title="Keywords" :content="$page.course.keywords" />
-      <syllabus-section title="Contents" :content="$page.course.contents" />
     </div>
   </syllabus-layout>
 </template>
@@ -39,13 +49,26 @@ query Course($id: ID!) {
   course: course(id: $id) {
     id
     title
+    start
+    practicalTeacher
+    credit
     time
-    purpose
+
     target
-    message
-    contents
-    finalTest
+    purpose
+    requirements
+    requirementsAdjusted
     gradePolicy
+    finalTest
+    task
+    contents
+
+    task
+    # textbooks
+    thingsToPrepare
+    references
+    message
+    contact
     category {
       id
       name
@@ -76,6 +99,15 @@ export default {
     tags() {
       const course = this.$page.course;
       return [
+        {
+          name: course.credit,
+          url: `#`,
+        },
+
+        {
+          name: course.start,
+          url: `#`,
+        },
         {
           name: course.time,
           url: `#`,
