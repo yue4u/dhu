e716827fd3e3216be54e7509d02b892b21c1ee4b
rainy-me <github@rainy.me>
2020-06-04 08:43:24 +0900

    fix: search data import

--- packages/syllabus-site/src/pages/Search.vue
+++ packages/syllabus-site/src/pages/Search.vue
@@ -2,15 +2,20 @@
   <syllabus-layout>
     <h1 class="syllabus-page-title">
       All
-      <span class="syllabus-page-title-count">{{ resultList.length }}</span>Results
+      <span class="syllabus-page-title-count">{{ resultList.length }}</span
+      >Results
     </h1>
 
     <input class="syllabus-input" placeholder="検索" v-model="searchText" />
 
     <ul class="syllabus-list">
-      <li class="syllabus-list-item search-item" v-for="result in resultList" :key="result.url">
+      <li
+        class="syllabus-list-item search-item"
+        v-for="result in resultList"
+        :key="result.url"
+      >
         <g-link class="search-link" :to="result.url">
-          <span class="search-title">{{result.title}}</span>
+          <span class="search-title">{{ result.title }}</span>
           <span class="search-content" v-html="result.text"></span>
         </g-link>
       </li>
@@ -19,12 +24,12 @@
 </template>
 
 <script>
-const { data } = require("../../data/search.json");
-
+const data = require("../../data/search.json");
+console.log(data);
 export default {
   data() {
     return {
-      searchText: ""
+      searchText: "",
     };
   },
   computed: {
@@ -35,12 +40,12 @@ export default {
 
       const searchText = this.searchText.toLowerCase();
       let result = data
-        .filter(item => {
+        .filter((item) => {
           item.index = item.text.toLowerCase().indexOf(searchText);
 
           return item.index !== -1;
         })
-        .map(item => {
+        .map((item) => {
           const text = item.text
             .substring(
               item.index - 10,
@@ -53,17 +58,17 @@ export default {
           return {
             text: `...${text}...`,
             title: item.title,
-            url: `/${item.type}/${item.id}`
+            url: `/${item.type}/${item.id}`,
           };
         });
 
       return result;
-    }
+    },
   },
   mounted() {
     const { q } = this.$route.query;
     this.searchText = q ? q : "";
-  }
+  },
 };
 </script>
 
