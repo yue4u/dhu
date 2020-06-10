19df6ef5585fca9e28750bff3b4243409032e9ad
rainy-me <github@rainy.me>
2020-06-11 06:57:04 +0900

    feat: rename helper function; use open page data

--- packages/syllabus-site/scripts/fetchData.ts
+++ packages/syllabus-site/scripts/fetchData.ts
@@ -1,10 +1,10 @@
 import path from "path";
 import { promises as fs } from "fs";
-import { getSyllabus, withPage } from "@dhu/core";
+import { getOpenSyllabus, withPage } from "@dhu/core";
 import { sortData } from "./sortData";
 
 (async () => {
-  const data = await withPage(getSyllabus);
+  const data = await withPage(getOpenSyllabus, { headless: false });
   await fs.writeFile(
     path.join(__dirname, "../data/syllabus.json"),
     JSON.stringify(sortData(data), null, 4),
